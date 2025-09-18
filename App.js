c3c941a60f62c1ebfea054a807667e7dca02580b
shivvenkatesh <shivvenkatesh69@gmail.com>
2025-09-18 13:50:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,59 +1,45 @@
-import React, {useState} from "react";
+
 import './App.css';
+import { 
+  Link,
+  Route,
+  Routes
+} from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 function App() {
-  const initialItems = [
-    { id: 1, name: "Apple" ,price:10},
-    { id: 2, name: "Mango" ,price:15},
-    { id: 3, name: "Banana", price:20},
-  ]
-  const [cart, setCart] = useState([]);
-  const [items] = useState(initialItems);
-
-  const addToCart = (item) => {
-    setCart([...cart, item]);
-  };
-  
-  const removeFromCart = (item) => {
-    const updatedCart=cart.filter((cartItem)=>  cartItem.id !== item.id);
-    setCart(updatedCart);
-  };
-
-  const calculateTotal = () => {
-    return cart.reduce((total, item) => total + item.price, 0);
-  };
-
   return (
     <div className="App">
-      <h1>Shopping Cart</h1>
-      <div className="items">
-        <h2>Available Items</h2>
-        <ul>
-          {items.map((item => (
-            <li key={item.id}>
-              {item.name} - ${item.price}
-              <button onClick={() => addToCart(item)}>Add to Cart</button>
-            </li>
-          )))}
-        </ul>
+      <div>
+        <div style={{
+          display : 'flex',
+          justifyContent: 'space-evenly',
+          background :' #baad351',
+          font:'20px'
+        }}>
+          <Link to ={'/'}
+          style={{color:'#000'}}>
+            Home
+          </Link>
+          <Link to ={'/About'}
+          style={{color:'#000'}}>
+            About
+          </Link>
+          <Link to ={'/Contact'}
+          style={{color:'#000'}}>
+            Contact
+          </Link>
+        </div>
       </div>
-      <div className="cart">
-        <ul>
-        <h2>Shopping cart</h2>
-        </ul>
-        <ul>
-          {cart.map((item) => (
-            <li key={item.id}>
-              {item.name} - ${item.price}
-              <button onClick={() => removeFromCart(item)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-        <h3>Total: ${calculateTotal()}</h3>
-      </div>
+      <Routes>
+        <Route  path ="/" element ={<Home/>}></Route>
+        <Route  path ="/about" element ={<About/>}></Route>
+        <Route  path ="/contact" element ={<Contact/>}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
-        
